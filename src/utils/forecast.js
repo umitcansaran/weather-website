e29838bb174359e1d85ba2449687ebef0b6ff908
Umit <umitcansaran@yahoo.com>
2020-08-23 17:42:56 +0200

    Add forecast data to resultt.

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions + ' It is currently ' + body.current.temperature + ' degress out. There is a ' + body.current.precip + '% chance of rain.')
+            callback(undefined, body.current.weather_descriptions + '. It is currently ' + body.current.temperature + ' degress out. There is a ' + body.current.precip + '% chance of rain. It feels like ' + body.current.feelslike + " degrees and the humidity is " + body.current.humidity + "%.")
         }
     })
 }
