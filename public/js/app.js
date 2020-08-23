a921f8f698fac2b75de94dc1a6713eb04cd8dd3d
Umit <umitcansaran@yahoo.com>
2020-08-23 11:34:18 +0200

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,24 @@
+console.log('Client side javascript file is loaded!')
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+let message1 = document.getElementById('message-1')
+let message2 = document.getElementById('message-2')
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    message1.innerHTML = "Loading ..."
+    message2.innerHTML = ""
+
+    fetch('http://localhost:3000/weather?address=' + search.value).then((response) => {
+    response.json().then((data) => {
+        if (data.error) {
+            message1.innerHTML = data.error
+        } else {
+            message1.innerHTML = data.location
+            message2.innerHTML = data.forecast
+        }
+    })
+})
+})
